c928a62f5e61dabcd184247b2d33d7f75540d352
Arjun Singh <101566727+ArjunSinghBhakuni@users.noreply.github.com>
2022-05-04 11:07:52 +0530

    Data

---
+++ data.js
@@ -0,0 +1,250 @@
+let data = {
+ men :[
+  {title:"BRUNELLO CUCINELLI",
+   price : 405,
+   img : "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/33/P00661762_b1.jpg",
+     
+  },
+  {title:"BRUNELLO CUCINELLI",
+   price : 429,
+   img : "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/09/P00661779_b1.jpg",
+     
+  },
+  {title:"TOM FORD",
+   price : 555,
+   img : "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/f9/P00611739_b1.jpg",
+     
+  },
+  {title:"GIVENCHY",
+   price : 605,
+   img : "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/e4/P00687175_b1.jpg",
+     
+  },
+  {title:"AURALEE",
+   price : 319,
+   img : "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/c5/P00662109_b1.jpg",
+     
+  },
+  {title:"TOM FORD",
+   price : 2945,
+   img : "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/79/P00611711.jpg",
+     
+  },
+  {title:"WALES BONNER",
+   price : 1135,
+   img : "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/49/P00653221_b1.jpg",
+     
+  },
+  {title:"LARDINI",
+   price : 835,
+   img : "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/2f/P00656715_b1.jpg",
+     
+  },
+  {title:"GUCCI",
+   price : 2105,
+   img : "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/12/P00613392_b1.jpg",
+     
+  },
+  {title:"SNOW PEAK",
+  price : 229,
+  img : "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/bd/P00665080.jpg",
+    
+ },
+ {title:"THE NORTH FACE",
+ price : 129,
+ img : "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/8d/P00652144_b1.jpg",
+   
+},
+{title:"HERNO",
+price : 475,
+img : "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/18/P00648673_b1.jpg",
+  
+},
+ ],
+
+
+
+
+ women : [
+  
+{title:"DOLCE&GABBANA ",
+price : 1219 ,
+img : " https://img.mytheresa.com/560/560/33/jpeg/catalog/product/95/P00680170_b1.jpg",
+  
+},
+ 
+{title:"CHRISTOPHER ESBER",
+price : 625 ,
+img : " https://img.mytheresa.com/560/560/33/jpeg/catalog/product/f1/P00648291_b1.jpg",
+  
+},
+ 
+{title:"DEVEAUX NEW YORK ",
+price  : 899  ,
+img : "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/6c/P00629755_b1.jpg ",
+  
+},
+ 
+{title:"GALVAN ",
+price : 465 ,
+img : "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/52/P00628692_b1.jpg ",
+  
+},
+ 
+{title:"DOLCE&GABBANA ",
+price :   585,
+img : "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/c2/P00678032_b1.jpg ",
+  
+},
+ 
+{title:"STAUD ",
+price : 209 ,
+img : " https://img.mytheresa.com/560/560/33/jpeg/catalog/product/00/P00549153_b1.jpg",
+  
+},
+ 
+{title:"LOEWE ",
+price :2695  ,
+img : " https://img.mytheresa.com/560/560/33/jpeg/catalog/product/e4/P00628715_b1.jpg",
+  
+},
+ 
+{title:" CANADA GOOSE",
+price :  779,
+img : "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/61/P00512165_b1.jpg  ",
+  
+},
+ 
+{title:" CANADA GOOSE",
+price :  885,
+img : "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/0d/P00512229_b1.jpg ",
+  
+},
+ 
+{title:" ZIMMERMANN",
+price : 1639 ,
+img : "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/fa/P00668229_b1.jpg ",
+  
+},
+ 
+{title:" PACO RABANNE",
+price : 1425 ,
+img : "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/9c/P00680544_b1.jpg ",
+  
+},
+ 
+{title:"NENSI DOJAKA ",
+price : 435 ,
+img : "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/24/P00673377_b1.jpg ",
+  
+},
+
+
+ ],
+ kids :[
+
+  {title:" CHLOÉ KIDS ",
+price :199 ,
+img : "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/a1/P00650380_b1.jpg ",
+  
+},
+
+{title:"  IL GUFO",
+price :105 ,
+img : "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/b1/P00646398_b1.jpg ",
+  
+},
+
+{title:"ZIMMERMANN KIDS  ",
+price : 95,
+img : "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/b4/P00685259_b1.jpg ",
+  
+},
+
+{title:"ZIMMERMANN KIDS",
+price :105 ,
+img : " https://img.mytheresa.com/560/560/33/jpeg/catalog/product/3f/P00685349_b1.jpg",
+  
+},
+
+{title:" GUCCI KIDS ",
+price :335 ,
+img : "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/c8/P00617792_b1.jpg ",
+  
+},
+
+{title:" BONPOINT ",
+price : 65,
+img : "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/52/P00658218.jpg ",
+  
+},
+
+{title:"GUCCI KIDS  ",
+price :31 ,
+img : " https://img.mytheresa.com/560/560/33/jpeg/catalog/product/1b/P00617720_b1.jpg",
+  
+},
+
+{title:"  MONCLER ENFANT",
+price : 229,
+img : " https://img.mytheresa.com/560/560/33/jpeg/catalog/product/cf/P00657238_b1.jpg",
+  
+},
+
+{title:"BAPE KIDS  ",
+price :195 ,
+img : "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/96/P00655849_b1.jpg ",
+  
+},
+
+{title:"GUCCI KIDS",
+price :269 ,
+img : "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/fd/P00657439_b1.jpg ",
+  
+},
+
+{title:" POLO RALPH LAUREN KIDS ",
+price : 79,
+img : " https://img.mytheresa.com/560/560/33/jpeg/catalog/product/d2/P00663527_b1.jpg",
+  
+},
+
+{title:"  MONCLER ENFANT",
+price : 269,
+img : " https://img.mytheresa.com/560/560/33/jpeg/catalog/product/7b/P00655370_b1.jpg",
+  
+},
+
+ ],
+}
+
+
+function clothing_container (){
+  return `
+  <div id="slidebar">
+  <p>Size</p>
+  <p>colour</p>
+  <p>Category</p>
+</div>
+<div id="clothing_box">
+  <div id="clothing_navbar">
+    <p id="mens_clothing">Mens</p>
+    <p id="womens_clothing">Womens</p>
+    <p id="kids_clothing">Kids</p>
+    <select name="" id="sort">
+      <option value="">Sort by</option>
+      <option value="lth">Price Low to High</option>
+      <option value="htl">Price High to low</option>
+    </select>
+  </div>
+
+  <div id="clothing_divs"></div>
+</div>
+  `
+}
+
+
+export {data , clothing_container};
+
+
+ 
